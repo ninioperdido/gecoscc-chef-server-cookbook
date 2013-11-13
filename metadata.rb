fb01311820d15d05f6c9e8fd7c91dc666a15a366
Roberto C. Morano <rcmova@gmail.com>
2013-11-13 19:33:36 +0100

    added metadata files

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'gecoscc-chef-server'
+maintainer       'Roberto C. Morano'
+maintainer_email 'rcmorano@emergya.com'
+license          'EUPL V 1.1 http://www.osor.eu/eupl'
+description      'Installs/Configures gecoscc-chef-server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
