2f93a97815c1e12ee1f5e3a175161f4d38ffde12
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-05-27 11:37:39 +0200

    Update default.rb
    Chef-server port from 449 to 443.

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,4 +12,4 @@ default['gecoscc-chef-server']['chef-server-version'] = '11.0.12'
 default['gecoscc-chef-server']['configuration']['nginx']['enable'] = true
 default['gecoscc-chef-server']['configuration']['nginx']['non_ssl_port'] = false
 default['gecoscc-chef-server']['configuration']['nginx']['enable_non_ssl'] = false
-default['gecoscc-chef-server']['configuration']['nginx']['ssl_port'] = 449
+default['gecoscc-chef-server']['configuration']['nginx']['ssl_port'] = 443
