9544761f74300ee2e6894e5e4c7a5c86467de67d
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-09-03 13:21:37 +0200

    Update chef-server to 11.1.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,7 +11,7 @@ require 'socket'
 
 server_name = UDPSocket.open {|s| s.connect('64.233.187.99', 1); s.addr.last }
 
-default['gecoscc-chef-server']['chef-server-version'] = '11.0.12'
+default['gecoscc-chef-server']['chef-server-version'] = '11.1.4'
 default['gecoscc-chef-server']['configuration']['nginx']['url'] = "https://#{server_name}"
 default['gecoscc-chef-server']['configuration']['nginx']['enable'] = true
 default['gecoscc-chef-server']['configuration']['nginx']['non_ssl_port'] = false
